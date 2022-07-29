088b45a0748a0db660b51ec0dbc9721378b9a4b2
Lucas Silva <lguilherme8@gmail.com>
2022-07-29 09:32:44 -0300

    add: campo imagem no modelo Produto, models.Product

--- core/models.py
+++ core/models.py
@@ -1,9 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.TextField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
