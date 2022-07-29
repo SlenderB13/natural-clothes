a8a79ed780926b33ffb562ef6a4c54c1bf78f5cc
Lucas Silva <lguilherme8@gmail.com>
2022-07-29 11:28:21 -0300

    add: navegação para página de cada produto

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from distutils import core
 from django.urls import path 
-from .views import home
+from .views import home, product
 
 urlpatterns = [
     path('', home),
+    path('product/<int:id>', product),
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from core.models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'core/home.html', context={'products': products})
+
+def product(request, id):
+    product = Product.objects.get(id=id)
+    return render(request, 'core/product.html', context={'product': product})
