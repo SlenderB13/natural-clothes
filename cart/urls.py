946da3facd869caccf48cf69828f25afc2e3e519
Lucas Silva <lguilherme8@gmail.com>
2022-08-01 09:52:28 -0300

    add: função de remover itens do carrinho

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import add_product, bag
+from .views import add_product, remove_product, bag
 
 urlpatterns = [
     path('add_product/<int:id>', add_product, name='add_product'),
+    path('remove_product/<int:id>', remove_product, name='remove_product'),
     path('bag', bag, name='bag'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from cart.models import Cart, Product
 
@@ -7,7 +7,12 @@ cart.save()
 
 def add_product(request, id):
     cart.products.add(Product.objects.get(id=id))
-    return render(request, 'core/home.html')
+    return render(request, 'cart/cart.html')
+
+def remove_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    cart.products.remove(product)
+    return render(request, 'cart/cart.html')
 
 def bag(request):
     products = cart.products.all()
