7844821cdcd2c1e46685a8918d0366bb16eb011d
Lucas Silva <lguilherme8@gmail.com>
2022-08-01 09:18:16 -0300

    add: página do carrinho renderizando itens

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import add_product
+from .views import add_product, bag
 
 urlpatterns = [
     path('add_product/<int:id>', add_product, name='add_product'),
+    path('bag', bag, name='bag'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -8,3 +8,7 @@ cart.save()
 def add_product(request, id):
     cart.products.add(Product.objects.get(id=id))
     return render(request, 'core/home.html')
+
+def bag(request):
+    products = cart.products.all()
+    return render(request, 'cart/cart.html', context={'products': products})
