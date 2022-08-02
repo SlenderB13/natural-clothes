e3b01e957074a8e0ccaa7cd444e5742edaab2b73
Lucas Silva <lguilherme8@gmail.com>
2022-08-02 13:42:57 -0300

    refact: funções passadas para o padrão django 3.1.7

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from distutils import core
-from django.urls import path
+from django.urls import path, include
 from .views import home, product
 
 urlpatterns = [
     path('', home, name='home'),
+    path('cart/', include('cart.urls')),
     path('product/<int:id>', product, name='product'),
 ]
