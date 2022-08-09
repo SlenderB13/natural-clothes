bf3d5c591b903d1b43dd3205737cd0137b9e73ea
Lucas Silva <lguilherme8@gmail.com>
2022-08-09 17:05:01 -0300

    add: validação dos inputs vazios

--- cart/static/cart/js/index.js
+++ cart/static/cart/js/index.js
@@ -1,14 +1,23 @@
 CEP_API_BASE_URL = 'https://viacep.com.br/ws'
 
+// get postal code
 $('#postal-code-input').keyup((e) => {
     this.code = $('#postal-code-input').val()
 })
 
+// use postal code to fetch with viaCep API
 $('#search-code-btn').click((e) => {
     e.preventDefault()
     fetch_address(this.code)
     .then((result) => {
         set_address(result.logradouro)
+    })
+})
+
+// get all inputs and validate each of them
+$('#finish-btn').click(() => {
+    $('input[type="text"]').each((index, input) => {
+        validate_input(input)
     })
 })
 
@@ -24,4 +33,13 @@ async function fetch_address(code) {
 
 function set_address(address) {
     $('#address-street-input').val(`${address}`)
+}
+
+function validate_input(input) {
+    if($(input).val() == '') {
+        $(input).addClass('is-invalid')
+    } else {
+        $(input).removeClass('is-invalid')
+        $(input).addClass('is-valid')
+    }
 }
