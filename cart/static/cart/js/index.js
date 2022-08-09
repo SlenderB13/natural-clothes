8a65ca6d33b2e8360addc9908973e8d0813169e6
Lucas Silva <lguilherme8@gmail.com>
2022-08-08 21:09:55 -0300

    add: função de consultar endereço via cep

--- cart/static/cart/js/index.js
+++ cart/static/cart/js/index.js
@@ -1,1 +1,23 @@
-console.log('whatever')
+CEP_API_BASE_URL = 'https://viacep.com.br/ws'
+
+$('.form-control').keyup((e) => {
+    this.code = $('.form-control').val()
+})
+
+$('#check-code-btn').click((e) => {
+    e.preventDefault()
+    fetch_address(this.code)
+    .then((result) => {
+        set_address(result.logradouro)
+    })
+})
+
+async function fetch_address(code) {
+    const response = await fetch(`${CEP_API_BASE_URL}/${code}/json`)
+    const result = await response.json() 
+    return result
+}
+
+function set_address(address) {
+    $('#address').text(`Shipping for: ${address}`)
+}
